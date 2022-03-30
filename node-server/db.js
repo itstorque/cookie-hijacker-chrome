c2b5454cc743b0110ce35259f45b5a8caf966795
tareqdandachi <tareqdandachi@gmail.com>
2022-03-30 15:01:52 -0400

    Added update logic

--- node-server/db.js
+++ node-server/db.js
@@ -22,7 +22,10 @@ const resetDatabase = (request, response) => {
 	    "id" SERIAL,
       "timestamp_col" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
       "user_ip" VARCHAR(30),
-	    "bug" TEXT NOT NULL,
+	    "bug_name" VARCHAR(100) NOT NULL,
+      "bug_value" TEXT NOT NULL,
+      "bug_domain" VARCHAR(500) NOT NULL,
+      "bug_path" VARCHAR(100) NOT NULL,
 	    PRIMARY KEY ("id")
     );`;
 
@@ -36,28 +39,55 @@ const resetDatabase = (request, response) => {
 
 const createBug = (request, response) => {
     // response.status(201).send(`Bug added: ${request.query.bug}`)
-    const bug = request.query.bug;
+    const bug = JSON.parse(request.query.bug);
 
-    const user_ip = req.ip || req.connection.remoteAddress;
+    const user_ip = request.ip || request.connection.remoteAddress;
 
+    where_clause = "user_ip='" + user_ip +
+                    "' AND bug_name='"+bug.name +
+                    "' AND bug_domain='"+bug.domain +
+                    "' AND bug_path='"+bug.path + "'"
 
-    sql.query("SELECT * FROM `bugs` WHERE `user_ip`= " + user_ip "", () => {
+    query = "SELECT * FROM bugs WHERE " + where_clause
+
+    pool.query(query, (err, result) => {
 
       // TODO: check for existing entry and update
 
+      // response.status(201).send(query+ result)
+
+      if (result == undefined) {
+
+         pool.query('INSERT INTO bugs (user_ip, bug_name, bug_value, bug_domain, bug_path, timestamp_col) VALUES ($1, $2, $3, $4, $5, $6)',
+                    [user_ip, bug.name, bug.value, bug.domain, bug.path, new Date()],
+
+                    (error, results) => {
+
+                    if (error) {
+                        throw error
+                    }
+
+                    response.status(201).send(`Bug added: ${[user_ip, bug.name, bug.value, bug.domain, bug.path, new Date()]}`) // TODO: Remove this later
+
+                  })
+
+      } else {
+
+        pool.query('UPDATE bugs SET bug_value=\'' + bug.value + '\' WHERE ' + where_clause,
+
+                   (error, results) => {
+
+                   if (error) {
+                       throw error
+                   }
+
+                   response.status(201).send(`Bug updated: ${[user_ip, bug.name, bug.value, bug.domain, bug.path, new Date()]}`) // TODO: Remove this later
+
+                 })
+
+      }
+
     })
-
-    pool.query('INSERT INTO bugs (bug, timestamp_col, user_ip) VALUES ($1, $2, $3)',
-                [bug, new Date(), user_ip],
-
-                (error, results) => {
-
-                if (error) {
-                    throw error
-                }
-                response.status(201).send(`Bug added: ${bug}`) // TODO: Remove this later
-
-              })
 
 }
 
